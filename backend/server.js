70f1d0cc73d9029ce415f8a18aa4038f602104e5
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-03-15 10:55:13 -0700

    Frontend Backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,32 @@
 // Write the Backend tasks here
+// 1
+// console.log("Server");
+// console.log("Server".split("").reverse().join(""));
+// console.log("Server".length);
+// console.log("Server".length * 3);
+// console.log("Server".length / 5);
+
+// 3
+let delay = 800;
+
+setTimeout(function() {
+    console.log("Server");
+}, delay);
+delay += 800;
+
+setTimeout(function() {
+    console.log("Server".split("").reverse().join(""));
+}, delay);
+delay += 800;
+
+setTimeout(function() {
+    console.log("Server".length);
+}, delay);
+delay += 800;
+
+setTimeout(function() {
+    console.log("Server".length * 3);
+}, delay);
+delay += 800;
+
+setTimeout(() => console.log("Server".length / 5), delay);
--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,32 @@
 // Write the Frontend tasks here
+// 2
+// console.log("Client");
+// console.log("Client".split("").reverse().join(""));
+// console.log("Client".length);
+// console.log("Client".length * 3);
+// console.log("Client".length / 5);
+
+// 3
+let delay = 800;
+
+setTimeout(function() {
+    console.log("Client");
+}, delay);
+delay += 800;
+
+setTimeout(function() {
+    console.log("Client".split("").reverse().join(""));
+}, delay);
+delay += 800;
+
+setTimeout(function() {
+    console.log("Client".length);
+}, delay);
+delay += 800;
+
+setTimeout(function() {
+    console.log("Client".length * 3);
+}, delay);
+delay += 800;
+
+setTimeout(() => console.log("Client".length / 5), delay);
